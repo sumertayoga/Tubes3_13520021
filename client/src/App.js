fb48b5edc38b3f8b3e74e744747f82524befef5b
3sulton <16520492@std.stei.itb.ac.id>
2022-04-29 10:51:36 +0700

    add regex for filtering riwayat

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import Axios from "axios";
 import './App.css';
 import Add from './feature/Add'
 import axios from 'axios';
@@ -9,29 +8,39 @@ import Nav from './Nav';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
-  
+
   const [riwayatList, setRiwayatList] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:3001/riwayat")
     .then((response) => {
+      for(let i = 0; i < response.data.length; i++) {
+        response.data[i].tanggal = moment.utc(response.data[i].tanggal).format('DD MMMM YYYY');
+      }
       setRiwayatList(response.data);
     })
   }, [])
   
+  const checkRegex = (dataItem, strInput) => {
+    var pattern = strInput.split("").map((x)=>{
+        return `(?=.*${x})`
+    }).join("");
+    var regex = new RegExp(`${pattern}`, "g")
+    return dataItem.match(regex);
+}
+
   const [searchInput, setSearchInput] = useState('');
   const [filteredResults, setFilteredResults] = useState([]);
   const searchItems = (searchValue) => {
     setSearchInput(searchValue)
     if (searchInput.length > 0) {
       const filteredData = riwayatList.filter((item) => {
-        return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+        return checkRegex([Object.values(item)[1], Object.values(item)[3]].join(' ').toLowerCase(), searchInput.toLowerCase())
       })
       setFilteredResults(filteredData)
     } else {
       setFilteredResults(riwayatList)
     }
   }
-
 
   return (
     <div className="App">
@@ -43,8 +52,8 @@ function App() {
         </Switch>
       </Router>
       <h1>Pencarian Riwayat</h1>
-      <input type="text" placeholder='Search...'
-          onChange={(e) => searchItems(e.target.value)}
+      <input className='search' type="text" placeholder='Search...'
+          onKeyUp={(e) => searchItems(e.target.value)}
           />
       {searchInput.length > 2 ? (
         filteredResults.map((item) => {
@@ -52,17 +61,17 @@ function App() {
             <div className='riwayat'>
               <div className='box' key={item.id}>
                 {/* no tanggal nama penyakit hasil */}
-                <p className='item'>{item.id_riwayat}</p>
-                <p className='item'>{moment.utc(item.tanggal).format('DD/MM/YY')}</p>
-                <p className='item'>{item.pengguna}</p>
-                <p className='item'>{item.nama}</p>
-                <p className='item'>{item.hasil}</p>
+                <p className='item_id'>{item.id_riwayat}</p>
+                <p className='item_tanggal'>{item.tanggal}</p>
+                <p className='item_pengguna'>{item.pengguna}</p>
+                <p className='item_penyakit'>{item.nama}</p>
+                <p className='item_hasil'>{item.hasil}</p>
               </div>
             </div>
           )
         })
       ) : (
-          <p>Hasil akan di sini</p>
+          <p>Hasil pencarian akan terlihat di sini</p>
         )}
       
     </div>
--- server/index.js
+++ server/index.js
@@ -23,67 +23,6 @@ app.get("/riwayat", (req, res) => {
     });
   });
 
-// Route to get all posts
-app.get("/api/get", (req,res)=>{
-db.query("SELECT * FROM posts", (err,result)=>{
-    if(err) {
-    console.log(err)
-    } 
-res.send(result)
-});   });
-
-// Route to get one post
-app.get("/api/getFromId/:id", (req,res)=>{
-
-const id = req.params.id;
- db.query("SELECT * FROM posts WHERE id = ?", id, 
- (err,result)=>{
-    if(err) {
-    console.log(err)
-    } 
-    res.send(result)
-    });   });
-
-// Route for creating the post
-app.post('/api/create', (req,res)=> {
-
-const username = req.body.userName;
-const title = req.body.title;
-const text = req.body.text;
-
-db.query("INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",[title,text,username], (err,result)=>{
-   if(err) {
-   console.log(err)
-   } 
-   console.log(result)
-});   })
-
-// Route to like a post
-app.post('/api/like/:id',(req,res)=>{
-
-const id = req.params.id;
-db.query("UPDATE posts SET likes = likes + 1 WHERE id = ?",id, (err,result)=>{
-    if(err) {
-   console.log(err)   } 
-   console.log(result)
-    });    
-});
-
-// Route to delete a post
-
-app.delete('/api/delete/:id',(req,res)=>{
-const id = req.params.id;
-
-db.query("DELETE FROM posts WHERE id= ?", id, (err,result)=>{
-if(err) {
-console.log(err)
-        } }) })
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`)
-})
-
-
 // SERVER FOR tesdna
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -214,3 +153,7 @@ app.post('/upload', (req, res) => {
 
     })
 });
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on ${PORT}`)
+})
